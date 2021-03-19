90e3afd3a8596df2b80d97782bd7bded9a19513a
5baddi <project@baddi.info>
2021-03-19 19:38:50 +0100

    Update front end

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,25 @@
-require('./bootstrap');
+require('./bootstrap')
 
 // Import dependencies
 import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
+// Import CSS files
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Pages components
 import Signin from './views/signin.vue'
+
+// Register dependencies
+Vue.use(BootstrapVue)
+
+// Register global components
+// Vue.component('BModal', BModal)
+// Vue.directive('b-modal', VBModal)
 
 // Init app
 const app = new Vue({
     el: '#app',
     components: { Signin }
-});
+})
