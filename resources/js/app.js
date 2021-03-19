3af7f6485d75000c94acd8860d45b0fa91d46ba2
5baddi <project@baddi.info>
2021-03-19 20:12:54 +0100

    Init app

---
+++ resources/js/app.js
@@ -0,0 +1,26 @@
+require('./bootstrap')
+
+// Import dependencies
+import Vue from 'vue'
+import { BootstrapVue, BContainer, BRow, BCol } from 'bootstrap-vue'
+
+// Import CSS files
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Pages components
+import Signin from './views/signin.vue'
+
+// Register dependencies
+Vue.use(BootstrapVue)
+
+// Register global components
+Vue.component('BContainer', BContainer)
+Vue.component('BRow', BRow)
+Vue.component('BCol', BCol)
+
+// Init app
+const app = new Vue({
+    el: '#app',
+    components: { Signin }
+})
