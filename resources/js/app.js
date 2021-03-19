96d96d798eb9bf40592516809d3e532b582630db
5baddi <project@baddi.info>
2021-03-19 19:15:31 +0100

    Update front end

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,10 @@ require('./bootstrap');
 import Vue from 'vue'
 
 // Pages components
-import App from './views/app.vue'
+import Signin from './views/signin.vue'
 
 // Init app
 const app = new Vue({
     el: '#app',
-    components: { App }
+    components: { Signin }
 });
