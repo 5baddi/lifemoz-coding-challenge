075431cf5ade1dd47b78ea635361e5eebede3395
Mohamed Baddi <project@baddi.info>
2021-03-20 13:31:27 +0100

    Init user repository

---
+++ app/Repositories/UsersRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Torann\LaravelRepository\Repositories\AbstractRepository;
+
+class UsersRepository extends AbstractRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    protected $model = \App\User::class;
+}
