b24b09629893262781b0ecd826c1c154ccb4780e
Mohamed Baddi <project@baddi.info>
2021-03-20 13:11:14 +0100

    Init auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function signIn()
+    {
+
+    }
+
+    public function signUp()
+    {
+
+    }
+    
+    public function forgotPassword()
+    {
+
+    }
+}
