73d8d69a64ced1261f9c413896aea0ed42dfc70b
Mohamed Baddi <project@baddi.info>
2021-03-23 14:11:55 +0100

    Fix dates format

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -53,8 +53,8 @@ class ReservationService
             [
                 'name'          =>  'required|string',
                 'room_id'       =>  'required|integer|exists:rooms,id',
-                'start_date'    =>  'required',
-                'end_date'      =>  'required',
+                'start_date'    =>  'required|date',
+                'end_date'      =>  'required|date',
                 'description'   =>  'nullable|string',
             ],
             [
